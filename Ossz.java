84a36a43b18e984812a63748842f5c3bc80790ff
csengekulker <k71608883429@ktch.hu>
2022-03-08 14:23:20 +0100

    user sets upperbound

--- Ossz.java
+++ Ossz.java
@@ -1,13 +1,21 @@
 import java.util.Random;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 class Ossz {
     public static void main(String[] args) {
         Random rand = new Random();
+        Scanner sc = new Scanner(System.in);
         ArrayList<Integer> numbers = new ArrayList<>();
 
+        System.out.print("Adja meg a tartomány felső határát: ");
+        int range = sc.nextInt();
+
+        sc.close();
+
+
         for (int i = 0; i<8;i++) {
-            int randomNumber = rand.nextInt(25) + 1;
+            int randomNumber = rand.nextInt(range) + 1;
 
             System.out.println("Szam:" + randomNumber);
 
