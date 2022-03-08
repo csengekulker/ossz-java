7e9e730807e4399cd4758c4a7e57368d331c4234
csengekulker <k71608883429@ktch.hu>
2022-03-08 14:19:45 +0100

    fill array with random ints

--- Ossz.java
+++ Ossz.java
@@ -1,11 +1,23 @@
+import java.util.Random;
+import java.util.ArrayList;
+
 class Ossz {
     public static void main(String[] args) {
-        int[] numbers = new int[8];
+        Random rand = new Random();
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        for (int i = 0; i<8;i++) {
+            int randomNumber = rand.nextInt(25) + 1;
+
+            System.out.println("Szam:" + randomNumber);
+
+            numbers.add(randomNumber);
+        }
 
         int sum = 0;
 
-        for (int i = 0; i<numbers.length;i++) {
-            sum += numbers[i];
+        for (int i = 0; i<numbers.size();i++) {
+            sum += numbers.get(i);
         }
 
         System.out.println("Az összeg: " + sum);
